6038cb04c50d31fa167010e283cad796dec18fb1
swayam <swayambansal01@gmail.com>
2025-08-11 11:22:37 -0400

    solved by creating a hashtable by arrays

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -11,11 +11,8 @@ class Solution:
 
         # now compare the given sorted strings
         # if they are equal then they are anagram, else just return false
-        if sorted_s == sorted_t:
-            return True
-
-        return False
-    
+        return sorted_s == sorted_t
+   
 
 # Solution 2: Use hashmaps ... 
 class Solution:
@@ -38,4 +35,26 @@ class Solution:
                 return False
             
         return True
+    
+
+# Solution 3: Usign arrays, making a hash table of all lower case letters
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counter = [0] * 26
+        for i in range(len(s)):
+            # ord('a') gives us the ASCII value of 'a', so we subtract it to get the index in the counter array
+            # if they are anagrams, the counts will cancel out to zero
+            # if not, the counts will not cancel out, and we will return false at the
+            counter[ord(s[i]) - ord('a')] += 1
+            counter[ord(t[i]) - ord('a')] -= 1
+
+        for val in counter:
+            if val != 0:
+                return False
+            
+        return True
+
         
